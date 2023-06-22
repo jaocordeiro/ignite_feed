0711ccfd027d47b6db22d8b1eb83bb24e97df578
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-06-21 22:04:54 -0300

    ajust component Post and styles

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,10 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
   return (
     <article className={styles.post}>
-      <header>
+      <header className={styles.header}>
         <div className={styles.author}>
           <img className={styles.avatar} src="https://github.com/jaocordeiro.png" />
           <div className={styles.authorInfo}>
@@ -15,11 +16,30 @@ export function Post() {
         <time title='20 de Junho as 21:56' dateTime='2023-06-20 21:56:12'>Publicado há 1h</time>
       </header>
 
-      <div>
+      <div className={styles.content}>
         <p>Fala galera</p>
         <p>Acabei de postar um novo artigo, espero que gostem, estudei e me apliquei muito no mesmo. vlw</p>
         <p><a href="#">site.dev/jaocordeiro</a></p>
-        <p><a href="#">#project #dev #rocketseat</a></p>
+        <p>
+          <a href="#">#project</a>{' '}
+          <a href="#">#dev </a>{' '}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder='Deixe seu comentário' />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentlist}>
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
 
     </article>
