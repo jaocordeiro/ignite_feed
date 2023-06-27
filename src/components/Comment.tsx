aa8d2eb98355abce596ea22edae38267288de021
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-06-26 21:29:19 -0300

    styles component Comment

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/jaocordeiro.png" alt="" />
+      <Avatar hasBorder={false} srcImg="https://github.com/jaocordeiro.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -15,7 +16,7 @@ export function Comment() {
             </div>
 
             <button title='Deletar comentário'>
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
