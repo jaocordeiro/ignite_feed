aef2caa3fcc24e3a1b7bbf9b93596c35724f1445
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-06-26 21:30:34 -0300

    ajusted styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 import { PencilLine } from 'phosphor-react'
 
@@ -10,7 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/jaocordeiro.png" />
+        <Avatar hasBorder srcImg="https://github.com/jaocordeiro.png" />
 
         <strong>Joao Vitor</strong>
         <span>Front-end Developer</span>
