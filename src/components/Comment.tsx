249fd750d714909ec8b5b4320d42f6cecd69ce78
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-07-08 17:43:07 -0300

    create textarea comment dinamic

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment() {
+interface CommentType {
+  content: string
+}
+
+export function Comment({ content }: CommentType) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} srcImg="https://github.com/jaocordeiro.png" />
@@ -20,7 +24,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom mano parabéns</p>
+          <p>{content}</p>
         </div>
 
         <footer>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar'
@@ -6,6 +7,9 @@ import { PostType } from '../App'
 import styles from './Post.module.css'
 
 export function Post({ author, content, publishedAt }: PostType) {
+  const [comments, setComments] = useState([])
+  const [newCommentText, setNewCommentText] = useState('')
+
   const publishedAtDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", {
     locale: ptBR,
   })
@@ -14,6 +18,17 @@ export function Post({ author, content, publishedAt }: PostType) {
     locale: ptBR,
     addSuffix: true
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event?.target.value)
+  }
 
   return (
     <article className={styles.post}>
@@ -41,19 +56,25 @@ export function Post({ author, content, publishedAt }: PostType) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder='Deixe seu comentário' />
+        <textarea
+          name='comment'
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+          placeholder='Deixe seu comentário' />
         <footer>
           <button type='submit'>Publicar</button>
         </footer>
       </form>
 
       <div className={styles.commentlist}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return (
+            <Comment content={comment} />
+          )
+        })}
       </div>
 
     </article>
